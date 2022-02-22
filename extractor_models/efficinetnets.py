d852ca93ca82dac58b2a59c563229254e142e161
Pengbo Wei <68414598+Samwei1@users.noreply.github.com>
2022-02-22 11:50:51 +1100

    upload feature extractors

---
+++ extractor_models/efficinetnets.py
@@ -0,0 +1,5 @@
+from efficientnet_pytorch import EfficientNet
+# Efficinetnet pytorch https://github.com/lukemelas/EfficientNet-PyTorch
+
+# efficinetnet_b0
+model = EfficientNet.from_pretrained('efficientnet-b0')
